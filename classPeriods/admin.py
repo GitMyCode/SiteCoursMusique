2a06c58d679e8f77fc8ea3255a9528a6c823db57
quantumlicht <guay.philippe@gmail.com>
2013-09-18 20:03:25 -0400

    removing classPeriods app

--- classPeriods/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from classPeriods.models import ClassPeriod
-
-class ClassPeriodAdmin(admin.ModelAdmin):
-    fields = ['teacher','instrument','start_time','end_time']
-    
-admin.site.register(ClassPeriod, ClassPeriodAdmin)
