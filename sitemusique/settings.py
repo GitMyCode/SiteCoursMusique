97a5b7e0a949555a832fc50eab727cc51b8a349d
mb <mb@ubuntu.(none)>
2013-09-23 09:16:52 -0700

    conf.edit

--- sitemusique/settings.py
+++ sitemusique/settings.py
@@ -1,4 +1,7 @@
 # Django settings for sitemusique project.
+import os
+import conf.py
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,17 +12,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
